f7622b624b1ebd5a368130a0651b80c6025bfd91
Nour Al Salhi <nuraym9998@gmail.com>
2023-10-01 14:58:04 +0300

    first commit

---
+++ src/main/java/org/example/LengthString.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class LengthString {
+    public int getLength (String name){
+        String text = name.trim();
+        return text.length();
+    }
+}
---
+++ src/test/java/LengthStringTest.java
@@ -0,0 +1,25 @@
+import org.example.LengthString;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LengthStringTest {
+    @DataProvider (name = "dataLength")
+    public Object [] [] dataString (){
+       return new Object[][] {
+               {"Nour",4},
+               {" Nour", 4},
+               {" Nour ",4},
+               {"No a ur",7},
+               {" ",0},
+               {"",0}
+
+       };
+    };
+    @Test(dataProvider = "dataLength")
+    public void testGetLength(String text,int expected) {
+       LengthString getLength1 = new LengthString();
+       int length = getLength1.getLength(text);
+       Assert.assertEquals(length,expected);
+    }
+}
