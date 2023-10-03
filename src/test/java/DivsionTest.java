d18e44261d955c684c69fa368ac7d042bdcaa7c5
Nour Al Salhi <nuraym9998@gmail.com>
2023-10-03 11:13:16 +0300

    add comment

--- src/test/java/DivsionTest.java
+++ src/test/java/DivsionTest.java
@@ -8,13 +8,13 @@ public class DivsionTest {
     @DataProvider(name = "Valid_Data")
     public Object [] [] validData (){
         return new Object[][] {
-                {4.0f,4.0f},
+                {4,4},
         };
     };
     @DataProvider(name = "Invalid_Data")
     public Object [] [] invalidData (){
         return new Object[][] {
-                {4.0f,0.0f},
+                {4,0},
         };
     };
     @Test(dataProvider = "Valid_Data")
@@ -22,6 +22,7 @@ public class DivsionTest {
         divisionNumber Divsion = new divisionNumber();
         Divsion.divsion(x,y);
     }
+    //use ArithmeticException when test invalid data
     @Test(dataProvider = "Invalid_Data",expectedExceptions = ArithmeticException.class)
     public void testInvalidData(float x, float y) {
         divisionNumber Divsion = new divisionNumber();
