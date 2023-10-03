2f604f4394457925c5d289fcdc7e79fc9f7c3442
Nour Al Salhi <nuraym9998@gmail.com>
2023-10-03 10:16:43 +0300

    use before/after method

--- src/test/java/AnnTest.java
+++ src/test/java/AnnTest.java
@@ -1,8 +1,14 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 //Default Priority 0
 public class AnnTest {
+    @BeforeMethod
+    public void printStatusBefore(){
+        System.out.println("Print Status");
+    }
     @Test(priority = 0)
     public void testSum(){
         Assert.assertTrue(true);
@@ -10,5 +16,9 @@ public class AnnTest {
     @Test(priority = 1)
     public void testDivision(){
         Assert.assertTrue(5==5);
+    }
+    @AfterMethod
+    public void printStatus(){
+        System.out.println("Pass");
     }
 }
